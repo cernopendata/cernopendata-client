b03de4fb7519864a1e0f1f1380467007c5a73b17
Tibor Šimko <tibor.simko@cern.ch>
2025-12-17 10:32:37 +0100

    refactor(validator): remove Python 2 compatibility code (#170)
    Remove dead code checking for Python 2 unicode type in
    validate_directory().

--- cernopendata_client/validator.py
+++ cernopendata_client/validator.py
@@ -117,9 +117,6 @@ def validate_directory(directory=None):
     :return: Bool after verifying directory
     :rtype: bool
     """
-    if sys.version_info[0] < 3:
-        if isinstance(directory, unicode):  # noqa F821
-            directory = directory.encode("utf-8")
     if isinstance(directory, str):
         if not directory.startswith("/eos/opendata/"):
             display_message(
