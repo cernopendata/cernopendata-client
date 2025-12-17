cc3bb8b80b6cf383168e0edf93ae17fafe65ab5c
Tibor Šimko <tibor.simko@cern.ch>
2025-12-17 10:32:43 +0100

    refactor(searcher): remove Python 2 compatibility code (#170)
    Remove try/except fallback for importing quote from urllib
    that is no longer necessary for Python 3.8+ project.

--- cernopendata_client/searcher.py
+++ cernopendata_client/searcher.py
@@ -13,14 +13,10 @@ from __future__ import print_function
 import sys
 import requests
 
+from urllib.parse import quote
+
 from .config import SERVER_HTTP_URI, SERVER_ROOT_URI, SERVER_HTTPS_URI
 from .printer import display_message
-
-try:
-    from urllib.parse import quote
-except ImportError:
-    # fallback for Python 2
-    from urllib import quote
 
 
 def verify_recid(server=None, recid=None):
